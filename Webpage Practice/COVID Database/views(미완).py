f80ea4bcee89d6b82ce5bc3f30abe04994c8c56e
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-12-20 00:18:03 +0900

    Update views(미완).py

--- Webpage Practice/COVID Database/views(미완).py
+++ Webpage Practice/COVID Database/views(미완).py
@@ -2,12 +2,7 @@ from django.db import connection
 from django.shortcuts import render, redirect
 import pandas as pd
 
-def display(request):
-    StudOutput = []
-    ProfOutput = []
-    countiesOutput = []
-    covidOutput = []
-
+def initial_page(request):
     with connection.cursor() as cursor:
         initQuery1 = "DELETE FROM students"
         cursor.execute(initQuery1)
@@ -18,6 +13,20 @@ def display(request):
         initQuery4 = "DELETE FROM covid"
         cursor.execute(initQuery4)
 
+    return render(request, 'myApp/index.html')
+
+def display(request):
+    StudOutput = []
+    ProfOutput = []
+    countiesOutput = []
+    covidOutput = []
+    Q1 = []
+    Q2 = []
+    Q3 = []
+    Q4 = []
+    Q5 = []
+
+    with connection.cursor() as cursor:
         studQuery = "SELECT * FROM students"
         cursor.execute(studQuery)
         fetchResultStud = cursor.fetchall()
@@ -30,9 +39,71 @@ def display(request):
         cursor.execute(countQuery)
         fetchResultCount = cursor.fetchall()
 
-        covidQuery = "SELECT * FROM covid"
+        covidQuery = "SELECT * FROM covid ORDER BY covid.patientID*1"
         cursor.execute(covidQuery)
         fetchResultCovid = cursor.fetchall()
+
+        Query1 = "SELECT students.county, ROUND(AVG(students.score), 2) " \
+                 "FROM students " \
+                 "GROUP BY students.county "
+        cursor.execute(Query1)
+        fetchResultQ1 = cursor.fetchall()
+
+        Query2 = "SELECT counties.city AS city, ROUND(AVG(score), 2) AS averageScore " \
+                 "FROM counties, students " \
+                 "WHERE students.county = counties.countyName " \
+                 "GROUP BY counties.city "
+        cursor.execute(Query2)
+        fetchResultQ2 = cursor.fetchall()
+
+        Query3 = "SELECT Profinfo.name, Studinfo.name " \
+                 "FROM (SELECT professors.name, Agetable.county" \
+                 "      FROM (SELECT professors.county, MAX(professors.age) AS elder_age" \
+                 "            FROM professors" \
+                 "            GROUP BY professors.county) Agetable, professors" \
+                 "      WHERE professors.age = Agetable.elder_age AND professors.county = Agetable.county) Profinfo, " \
+                 "      (SELECT students.name, Scoretable.county" \
+                 "       FROM (SELECT students.county, MAX(students.score) AS highest_score" \
+                 "             FROM students" \
+                 "             GROUP BY students.county) Scoretable, students" \
+                 "       WHERE students.score = Scoretable.highest_score AND students.county = Scoretable.county) Studinfo " \
+                 "WHERE Profinfo.county = Studinfo.county "
+        cursor.execute(Query3)
+        fetchResultQ3 = cursor.fetchall()
+
+        Query4 = "SELECT Profinfo.name, Studinfo.name " \
+                 "FROM (SELECT professors.name, Agetable.city " \
+                 "      FROM (SELECT counties.city, newTable.elder_age, counties.countyName " \
+                 "            FROM (SELECT counties.city, MAX(professors.age) AS elder_age " \
+                 "	                FROM professors JOIN counties " \
+                 "	                ON professors.county = counties.countyName " \
+                 "		            GROUP BY counties.city) newTable JOIN counties " \
+                 "	          ON counties.city = newTable.city) Agetable, professors " \
+                 "      WHERE professors.age = Agetable.elder_age AND professors.county = Agetable.countyName) Profinfo, " \
+                 "     (SELECT students.name, Scoretable.city" \
+                 "      FROM (SELECT counties.city, newStudTable.highest_score, counties.countyName " \
+                 "	          FROM (SELECT counties.city, MAX(students.score) AS highest_score " \
+                 "		            FROM students JOIN counties " \
+                 "		            ON students.county = counties.countyName " \
+                 "                  GROUP BY counties.city) newStudTable JOIN counties " \
+                 "            ON counties.city = newStudTable.city) Scoretable, students " \
+                 "      WHERE students.score = Scoretable.highest_score AND students.county = Scoretable.countyName) Studinfo " \
+                 "WHERE Profinfo.city = Studinfo.city "
+        cursor.execute(Query4)
+        fetchResultQ4 = cursor.fetchall()
+
+        Query5 = "SELECT students.name, TOP_three.city " \
+                 "FROM (SELECT patient_table.city, patient_table.patient_number / SUM(counties.population) AS ratio " \
+                 "      FROM (SELECT covid.city, COUNT(covid.city) as patient_number " \
+                 "            FROM covid " \
+                 "            GROUP BY covid.city) patient_table, counties " \
+                 "      WHERE counties.city = patient_table.city " \
+                 "      GROUP by counties.city " \
+                 "      ORDER BY ratio DESC " \
+                 "      LIMIT 3) TOP_three, students, counties " \
+                 "WHERE counties.city = TOP_three.city AND students.county = counties.countyName "
+        cursor.execute(Query5)
+        fetchResultQ5 = cursor.fetchall()
 
         connection.commit()
         connection.close()
@@ -53,13 +124,34 @@ def display(request):
             eachRow = {'patientID': temp[0], 'city': temp[1]}
             covidOutput.append(eachRow)
 
-    return render(request, 'myApp/index.html', {'students':StudOutput, 'professors':ProfOutput, 'counties':countiesOutput, 'covid':covidOutput})
+        for temp in fetchResultQ1:
+            eachRow = {'countyName':temp[0], 'averageScore':temp[1]}
+            Q1.append(eachRow)
 
-def display_stud(request):
-    stud_csv = pd.read_csv("./myApp/templates/myApp/Students.csv")
+        for temp in fetchResultQ2:
+            eachRow = {'city':temp[0], 'averageScore':temp[1]}
+            Q2.append(eachRow)
+
+        for temp in fetchResultQ3:
+            eachRow = {'profname':temp[0], 'studname':temp[1]}
+            Q3.append(eachRow)
+
+        for temp in fetchResultQ4:
+            eachRow = {'profname':temp[0], 'studname':temp[1]}
+            Q4.append(eachRow)
+
+        for temp in fetchResultQ5:
+            eachRow = {'studname':temp[0], 'city':temp[1]}
+            Q5.append(eachRow)
+
+    return render(request, 'myApp/index.html', {'students':StudOutput, 'professors':ProfOutput,
+                                                'counties':countiesOutput, 'covid':covidOutput, 'Q1':Q1, 'Q2':Q2, 'Q3':Q3, 'Q4':Q4, 'Q5':Q5})
+
+def insert_stud(request):
+    stud_csv = pd.read_csv("./myApp/templates/myApp/students.csv", header=None)
     stud_csv.columns = ['studentID', 'name', 'score', 'county']
 
-    for i in range(0, len(stud_csv)):
+    for i in range(len(stud_csv)):
         studID = stud_csv.iloc[i]['studentID']
         name = stud_csv.iloc[i]['name']
         score = float(stud_csv.iloc[i]['score'])
@@ -74,11 +166,11 @@ def display_stud(request):
 
     return redirect('index')
 
-def display_prof(request):
-    prof_csv = pd.read_csv("./myApp/templates/myApp/professors.csv")
+def insert_prof(request):
+    prof_csv = pd.read_csv("./myApp/templates/myApp/professors.csv", header=None)
     prof_csv.columns = ['facultyID', 'name', 'age', 'county']
 
-    for i in range(0, len(prof_csv)):
+    for i in range(len(prof_csv)):
         facID = prof_csv.iloc[i]['facultyID']
         name = prof_csv.iloc[i]['name']
         age = int(prof_csv.iloc[i]['age'])
@@ -93,11 +185,11 @@ def display_prof(request):
 
     return redirect('index')
 
-def display_counties(request):
-    count_csv = pd.read_csv("./myApp/templates/myApp/counties.csv")
+def insert_counties(request):
+    count_csv = pd.read_csv("./myApp/templates/myApp/counties.csv", header=None)
     count_csv.columns = ['countyName', 'population', 'city']
 
-    for i in range(0, len(count_csv)):
+    for i in range(len(count_csv)):
         cntName = count_csv.iloc[i]['countyName']
         popul = int(count_csv.iloc[i]['population'])
         city = count_csv.iloc[i]['city']
@@ -111,12 +203,12 @@ def display_counties(request):
 
     return redirect('index')
 
-def display_covid(request):
+def insert_covid(request):
     covidOutput = []
-    covid_csv = pd.read_csv("./myApp/templates/myApp/COVID.csv")
+    covid_csv = pd.read_csv("./myApp/templates/myApp/covid.csv", header=None)
     covid_csv.columns = ['patientID', 'city']
 
-    for i in range(0, len(covid_csv)):
+    for i in range(len(covid_csv)):
         patID = covid_csv.iloc[i]['patientID']
         city = covid_csv.iloc[i]['city']
 
